12cbde2f19fc40ce583c57a73803c4f5f03b3708
Yura Choi <yoorachoi8937@gmail.com>
2020-02-16 13:50:47 +0900

    Add references to README.md

---
+++ load_model.py
@@ -0,0 +1,10 @@
+from transformers import DistilBertModel, DistilBertModelfrom transformers import DistilBertModel, BertModel
+
+def get_distilkobert_model():
+    model = DistilBertModel.from_pretrained('monologg/distilkobert')
+    return model
+
+
+def get_kobert_model():
+    model = BertModel.from_pretrained('monologg/kobert')
+    return model
